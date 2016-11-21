a13830db3097c79c24a4ec3be7a02ded47395683
antar5 <tonmoyislam1@gmail.com>
2016-11-20 20:01:17 -0500

    dignitas webpage

---
+++ js/menu.js
@@ -0,0 +1,18 @@
+$(function(){
+	var obj = document.querySelectorAll('.nav-icon');
+	for(var i=obj.length-1; i>=0; i--){
+		var toggle = obj[i];
+		toggleSwitch(toggle);
+	}
+
+	function toggleSwitch(toggle){
+		toggle.addEventListener('click',function(){
+			if(this.classList.contains('active') === true){
+				this.classList.remove('active');
+			}
+			else {
+				this.classList.add('active');
+			}
+		})
+	}
+});
