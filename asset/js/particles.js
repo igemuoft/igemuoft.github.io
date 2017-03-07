cfb83d7811577df5269db824b03762c202972ce7
protoxpire0 <fahim.shahreer95@gmail.com>
2017-03-07 18:05:17 -0500

    igem website

--- asset/js/particles.js
+++ asset/js/particles.js
@@ -1,7 +1,7 @@
 particlesJS("particles-js", {
     "particles": {
         "number": {
-            "value": 25,
+            "value": 35,
             "density": {
                 "enable": true,
                 "value_area": 800
