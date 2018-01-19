3eca9c509ed71ad4c45f110970b9e0b51bd494db
Fahim Shahreer <fahim.shahreer95@gmail.com>
2018-01-19 14:08:28 -0500

    gulp: handlebars, sass, js

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,34 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass')
-
-
+var handlebars = require('gulp-compile-handlebars');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var ugilfy = require('gulp-uglify'); 
 // BROWSERSYNC TASKS
+
+// handlebars 
+gulp.task('handlebars', function () {
+  var templateData = {
+  },
+  options = {
+  	ignorePartials: true, 
+    partials : {
+    },
+    batch : ['./src/handlebars/partials'],
+    helpers : {
+    }
+  }
+ 
+  return gulp.src('src/handlebars/*.handlebars')
+    .pipe(handlebars(templateData, options))
+		.pipe(rename(function(path) {
+			path.extname = '.html'
+		}))
+    .pipe(gulp.dest('src'))
+		.pipe(browserSync.stream());
+});
+
 // compile sass to src/css
 gulp.task('sass', function() {
 	gulp.src('src/scss/*.scss')
@@ -14,18 +39,30 @@ gulp.task('sass', function() {
 		.pipe(browserSync.stream());
 })
 
+// concat and ugilfy scripts
+gulp.task('scripts', function() {
+	gulp.src('src/js/*.js')
+		.pipe(concat('main.js'))
+		.pipe(ugilfy())
+		.pipe(gulp.dest('src'))
+		.pipe(browserSync.stream());
+})
+
 // serving with browserSync
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['handlebars', 'sass', 'scripts'], function() {
 	browserSync.init({
 		server: 'src',
 	});
-	
-	gulp.watch(['src/scss/*.scss'], ['sass']);
-	gulp.watch(['src/*.html']).on('change', browserSync.reload);
+
+	gulp.watch(['src/handlebars/**'], ['handlebars']);
+	gulp.watch(['src/scss/**'], ['sass']);
+	gulp.watch(['src/js/*.js'], ['scripts']);
+	gulp.watch(['src/img/**']), browserSync.reload;
 });
 
 
 // BUILD TASKS
+
 // move all html to dist
 gulp.task('copyHTML', function() {
 	gulp.src('src/*.html')
